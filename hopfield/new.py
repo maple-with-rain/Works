02dbe5f9dbf0721d5e5dd6cd03a011a92267b9b6
maple-with-rain <1120242045@bit.edu.cn>
2025-09-07 11:53:11 +0800

    move

---
+++ hopfield/new.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+import torch.optim as optim
+class My_Moudule(nn.Module):
+    def __init__(self, liers , output):
+        super().__init__()
+    
